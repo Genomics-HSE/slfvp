11cf0dbde3b7777b6f17b8245ee2d49167bc1801
Nikita Gayanov <nikitaminecraft@gmail.com>
2024-01-29 01:32:44 +0300

    update

--- SLFVP.pyx
+++ SLFVP.pyx
@@ -98,46 +98,31 @@ cdef class Events:
         self.size = iterations
 
 
-
-cdef class Individual:
-    cdef public:
-        Py_ssize_t i_id, p_id
-        Py_ssize_t[:] i_type
-        double x, y, time, death_time
-        
-    def __init__(self, Py_ssize_t p_id, Py_ssize_t i_id, double time, double x, double y, Py_ssize_t[:] i_type, double death_time=-1):
-        self.p_id = p_id
-        self.i_id = i_id
-        self.time = time
-        self.x = x
-        self.y = y
-        self.i_type = i_type
-        self.death_time = death_time
-        
-    def __str__(self):
-        return f'p_id:{self.p_id}, i_id:{self.i_id}, x:{self.x},y:{self.y}'
-
-
 cdef class State:
     cdef public:
         Py_ssize_t max_size, size, n_alive, n_dead
-        Py_ssize_t[:] ids, p_ids, alive_ids
-        int n_alleles
+        Py_ssize_t[:] ids, p_ids, ids_alive, ids_to_die
+        Py_ssize_t n_alleles
         double[:] xs, ys, times, death_times
         Py_ssize_t[:, :] genotypes
         
         
-    def __init__(self, rho, u0, t, n_alleles, l=1):
+    def __init__(self, double rho, double u0, Py_ssize_t t, Py_ssize_t n_alleles, double l=1):
         self.size = 0
-        self.max_size = 2 * (rho+ u0 * rho t) * l**2
-        self.ids = np.array((max_size))
-        self.p_ids = np.array((max_size))
-        self.xs = np.array((max_size))
-        self.ys = np.array((max_size))
-        self.genotypes = np.array((max_size, n_alleles))
+        self.max_size = int(2 * (rho + u0 * rho * t) * l**2)
+        self.ids = np.empty(self.max_size, dtype = int)
+        self.p_ids = np.empty(self.max_size, dtype = int)
+        self.xs = np.empty(self.max_size)
+        self.ys = np.empty(self.max_size)
+        self.times = np.empty(self.max_size)
+        self.death_times = np.empty(self.max_size)
+        self.genotypes = np.empty((self.max_size, n_alleles), dtype = int)
         self.n_alive = 0
         self.n_dead = 0
         self.n_alleles = n_alleles
+        
+        self.ids_alive = np.empty(self.max_size, dtype = int)
+        self.ids_to_die = np.empty(self.max_size, dtype = int)
     
     @cython.boundscheck(False)
     @cython.wraparound(False)
@@ -145,41 +130,44 @@ cdef class State:
         
         # self.individuals[self.size] = Individual(p_id, self.size, time, x, y, i_type)
         
-        self.alive_ids[n_alive] = self.size
-        self.ids[self.size] = size
+        # self.alive_ids[self.n_alive] = self.size
+        self.ids[self.size] = self.size
         self.p_ids[self.size] = p_id
         self.xs[self.size] = x
         self.ys[self.size] = y
         self.times[self.size] = time
-        for k in self.n_alleles:
+        for k in range(self.n_alleles):
             self.genotypes[self.size, k] = i_type[k]
         self.size += 1
         self.n_alive += 1
     
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    cdef void remove(self, Py_ssize_t i_id, death_time):
-        self.n_alive -= 1
-        self.n_dead += 1
-        self.individuals[i_id].death_time = death_time
-        self.ids_alive.remove(i_id)
-        self.ids_dead.add(i_id)
+#     @cython.boundscheck(False)
+#     @cython.wraparound(False)
+#     cdef void remove(self, Py_ssize_t i_id, death_time):
+#         self.n_alive -= 1
+#         self.n_dead += 1
+#         self.individuals[i_id].death_time = death_time
+#         self.ids_alive.remove(i_id)
+#         self.ids_dead.add(i_id)
         
         
     cpdef void generate(self, double L, double rho, Py_ssize_t n_alleles, double proport):
-        cdef long i
+        cdef Py_ssize_t i
         N_points = np.random.poisson(rho*L**2)
         xs = np.random.uniform(0, L, N_points)
         ys = np.random.uniform(0, L, N_points)
         alleles = np.random.choice([0, 1], (N_points, n_alleles), p = [1 - proport, proport])
         for i in range(N_points):
-            self.add(
-                p_id = 0,
-                time = 0,
-                x = xs[i],
-                y = ys[i],
-                i_type = alleles[i]
-            )
+            self.ids_alive[self.size] = self.size
+            self.ids[self.size] = self.size
+            self.p_ids[self.size] = -1
+            self.xs[self.size] = xs[i]
+            self.ys[self.size] = ys[i]
+            self.times[self.size] = 0
+            for k in range(self.n_alleles):
+                self.genotypes[self.size, k] = alleles[i,k]
+            self.size += 1
+            self.n_alive += 1
         
    
     
@@ -200,13 +188,14 @@ cdef class SLFVP:
     cdef public:
         RndmWrapper seed
         double L, rho, lamda, theta, alpha, time, u0, integral, beta
-        Py_ssize_t n_alleles
-        double[:] probs
+        Py_ssize_t n_alleles, n_epoch
+        double [:] probs
+        Py_ssize_t [:] ids_newborn
         State state
         Events events
         double [:,:] points 
         
-    def __init__(self, double L=1, double lamda=1, double u0=0.4, double rho=1000, double theta=0.5, double alpha=1, Py_ssize_t n_alleles=10):
+    def __init__(self, double L=1, double lamda=1, double u0=0.4, double rho=1000, double theta=0.5, double alpha=1, Py_ssize_t n_alleles=10, t = 1000):
         self.rho=rho
         self.L=L
         self.u0=u0
@@ -214,11 +203,13 @@ cdef class SLFVP:
         self.alpha=alpha
         self.lamda = lamda
         self.beta = 1
-        self.state = State()
+        self.state = State(rho, u0, t, n_alleles, L)
+        self.n_epoch = t
         self.n_alleles = n_alleles
         self.time = 0
         self.integral = dblquad(lambda x, y: self.u(self.L/2, self.L/2, x,y), 0, self.L, 0,  self.L )[0]
         self.probs = np.empty(int(rho*L**2)*3 + 1)
+        self.ids_newborn = np.empty(int(rho*L**2)*3 + 1, dtype = int)
         self.seed = RndmWrapper(seed=(123, 0))
         self.points = np.empty((int(rho*L**2)*10,3), dtype = float)
         self.events = Events()
@@ -227,19 +218,20 @@ cdef class SLFVP:
     @cython.wraparound(False)
     @cython.cdivision(True)
     @cython.nonecheck(False)
-    cpdef Individual choose_parent(self, double z1, double z2):
+    cpdef Py_ssize_t choose_parent(self, double z1, double z2):
         
         cdef double sum_p = 0
         cdef Py_ssize_t i, p_id
-        cdef list list_alive = list(self.state.ids_alive)
-        for i in range(self.state.n_alive):
+        for i in self.state.ids_alive:
+            if i == -1:
+                break
             self.probs[i] = self.v(z1, z2,
-                                    self.state.individuals[list_alive[i]].x, self.state.individuals[list_alive[i]].y)
+                                    self.state.xs[i], self.state.ys[i])
             sum_p += self.probs[i]
         for i in range(self.state.n_alive):
             self.probs[i] /= sum_p
         p_id = random_choice(self.probs)
-        return self.state.individuals[list_alive[p_id]]
+        return self.state.ids_alive[p_id]
     
     
     
@@ -248,15 +240,19 @@ cdef class SLFVP:
     @cython.cdivision(True)
     cpdef void extinction(self, double z1, double z2, double time):
         cdef double x1, x2
-        cdef Py_ssize_t i
-        cdef list ids
-        cdef list list_alive = list(self.state.ids_alive)
-        for i in list_alive:
-            x1 = self.state.individuals[i].x
-            x2 = self.state.individuals[i].y
+        cdef Py_ssize_t i, k=0
+        for i in self.state.ids_alive:
+            if i == -1:
+                break
+            x1 = self.state.xs[i]
+            x2 = self.state.ys[i]
             if drand48() < self.u(z1, z2, x1, x2):
-                self.state.remove(i, time)
-                
+                self.state.ids_to_die[k] = i
+                k+=1
+            
+        self.state.ids_to_die[k] = -1
+        
+        self.state.n_alive -= k
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
@@ -264,11 +260,9 @@ cdef class SLFVP:
     cpdef void recolonization(self, double z1, double z2, double time):
         cdef Py_ssize_t n_points, generated, k,
         cdef double x1, x2
-        cdef Individual parent
         cdef Py_ssize_t[:] i_type
-        parent = self.choose_parent(z1, z2)
-        p_id = parent.i_id
-        i_type = np.copy(parent.i_type)
+        p_id = self.choose_parent(z1, z2)
+        i_type = self.state.genotypes[p_id]
         # for k in range(self.n_alleles):
             # i_type[k] = 
         max_intensity = self.u(z1, z2, z1, z2)
@@ -283,12 +277,55 @@ cdef class SLFVP:
 
             if self.L**2 * self.u(z1, z2, x1, x2) >= max_intensity * drand48():
                 self.state.add(p_id, time, x1, x2, i_type)
+                self.ids_newborn[generated] = self.state.size
+                self.state.size += 1
                 generated += 1
                 
+        self.ids_newborn[generated] = -1
+        
+        self.state.n_alive += n_points
                 
+    
+    cpdef void shift(self):
+        cdef Py_ssize_t length, k = 0, s = 1
+        length = len(self.state.ids_alive)
+        
+        for i in self.state.ids_to_die:
+            if i == -1:
+                break
+            if self.state.ids_alive[i] == -1:
+                break
+        if i != -1 and  self.state.ids_alive[i] == -1:
+            s = i+1
+            while i < self.state.n_alive:
+                while self.state.ids_alive[s] == -1:
+                    s += 1
+                self.state.ids_alive[i] = self.state.ids_alive[s]
+                
+        
+        
+    cpdef replace(self):
+        cdef Py_ssize_t length1, length2, k=0, j=0, s=0
+        length1 = len(self.state.ids_to_die)
+        length2 = len(self.state.ids_alive)
+        length3 = len(self.ids_newborn)
+        
+        while self.state.ids_to_die[k]!= -1 and self.ids_newborn[j]!= -1 and k < length1:
+            self.state.ids_alive[self.state.ids_to_die[k]] = self.ids_newborn[j]
+            k+=1
+            j+=1
+        while k+s<length2 and self.ids_newborn[j]!= -1 and j < length3:
+            self.state.ids_alive[self.state.ids_to_die[k+s]] = self.ids_newborn[j]
+            j+=1
+            s+=1
+        
+        
+    
     cpdef void propagate(self, double z1, double z2, double time):# parameters -- list [L, rho, u0, alpha, theta]
         self.extinction(z1, z2, time)
         self.recolonization(z1, z2, time)
+        self.replace()
+        self.shift()
     
     
     cpdef void run(self):
@@ -298,9 +335,10 @@ cdef class SLFVP:
         
         
         
-    cpdef void initiate(self, Py_ssize_t iterations, double proport):
-        self.events.CreateEvents(iterations, self.lamda, self.L)
+    cpdef void initiate(self, double proport):
+        self.events.CreateEvents(self.n_epoch, self.lamda, self.L)
         self.state.generate(self.L, self.rho, self.n_alleles, proport)
+        
         
     @cython.cdivision(True)    
     cdef inline double v(self, double z1, double z2, double x1, double x2):
@@ -418,14 +456,14 @@ cdef class SLFVP:
         ys1 = list()
         ys2 = list()
     
-        list_alive = list(self.state.ids_alive)
-        for i in range(self.state.n_alive):
-            if self.state.individuals[list_alive[i]].i_type[allele] == 0:
-                xs1.append(self.state.individuals[list_alive[i]].x)
-                ys1.append(self.state.individuals[list_alive[i]].y)
+
+        for i in range(self.state.ids_alive):
+            if self.state.genotypes[i, allele] == 0:
+                xs1.append(self.state.xs[i])
+                ys1.append(self.state.ys[i])
             else:
-                xs2.append(self.state.individuals[list_alive[i]].x)
-                ys2.append(self.state.individuals[list_alive[i]].y)
+                xs2.append(self.state.xs[i])
+                ys2.append(self.state.ys[i])
         plt.scatter(xs1, ys1, alpha, label ='0 allele')
         plt.scatter(xs2, ys2, alpha, label = '1 allele')
         plt.legend()
@@ -443,20 +481,21 @@ cdef class SLFVP:
         ys3 = list()
         ys4 = list()
     
-        list_alive = list(self.state.ids_alive)
-        for i in range(self.state.n_alive):
-            if self.state.individuals[list_alive[i]].i_type[0] == 0 and self.state.individuals[list_alive[i]].i_type[1] == 0 :
-                xs1.append(self.state.individuals[list_alive[i]].x)
-                ys1.append(self.state.individuals[list_alive[i]].y)
-            elif self.state.individuals[list_alive[i]].i_type[0] == 0 and self.state.individuals[list_alive[i]].i_type[1] == 1:
-                xs2.append(self.state.individuals[list_alive[i]].x)
-                ys2.append(self.state.individuals[list_alive[i]].y)
-            elif self.state.individuals[list_alive[i]].i_type[0] == 1 and self.state.individuals[list_alive[i]].i_type[1] == 0:
-                xs3.append(self.state.individuals[list_alive[i]].x)
-                ys3.append(self.state.individuals[list_alive[i]].y)
+        for i in list(self.state.ids_alive):
+            if i == -1:
+                break
+            if self.state.genotypes[i, 0] == 0 and self.state.genotypes[i, 1] == 0 :
+                xs1.append(self.state.xs[i])
+                ys1.append(self.state.ys[i])
+            elif self.state.genotypes[i, 0] == 0 and self.state.genotypes[i, 1] == 1:
+                xs2.append(self.state.xs[i])
+                ys2.append(self.state.ys[i])
+            elif self.state.genotypes[i, 0] == 1 and self.state.genotypes[i, 1] == 0:
+                xs3.append(self.state.xs[i])
+                ys3.append(self.state.ys[i])
             else:
-                xs4.append(self.state.individuals[list_alive[i]].x)
-                ys4.append(self.state.individuals[list_alive[i]].y)
+                xs4.append(self.state.xs[i])
+                ys4.append(self.state.ys[i])
         plt.scatter(xs1, ys1, alpha, label ='00 allele')
         plt.scatter(xs2, ys2, alpha, label = '01 allele')
         plt.scatter(xs3, ys3, alpha, label = '10 allele')
